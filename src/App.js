d8da314c49573605925b8ec088c2e1fb3ea6b6bd
israt jahan fariha <isratfariha71@gmail.com>
2022-11-25 23:12:03 +0600

    reason

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './Component/home/Home'
 import Program from './Component/programs/Programs'
+import Reasons from './Component/reasons/Reasons'
 export default function App(){
 
   return(
@@ -8,6 +9,7 @@ export default function App(){
     <div className='App'>
     <Hero />
     <Program />
+    <Reasons />
   </div>
 
     </>
